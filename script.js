e1b1f5838ef73b327b48f604a9987863d9e6ff25
Daniel Byun <dbyun92@gmail.com>
2017-02-18 18:53:16 -0500

    Change script.js
    formatted, and better display the todos with displayToDo method

--- script.js
+++ script.js
@@ -1,50 +1,54 @@
 var toDoList = {
+    
+    toDo: [],
+    
+    displayToDo: function(){
+        
+        console.log("My ToDos: ");
+        
+        for (var i = 0; i < this.toDo.length; i++){
+            console.log(this.toDo[i].toDoText);
+        }
+    
+    },
+    
+    addToDo: function(toDoText){
+        
+        this.toDo.push({
+      
+            toDoText: toDoText,
+            completed: false
+      
+        });
+    
+        this.displayToDo();
 
-  toDo: [],
- 
-  displayToDo: function(){
-    
-      console.log(this.toDo);
-    
-  },
+    },
   
-  addToDo: function(toDoText){
+    changeToDo: function(position, toDoText){
     
-      this.toDo.push({
-          
-          toDoText: toDoText,
-          completed: false
-
-      });
-
-      this.displayToDo();
-
-  },
+        this.toDo[position].toDoText = toDoText;
+        this.displayToDo();
+    
+    },
   
-  changeToDo: function(position, toDoText){
+    deleteToDo: function(position){
     
-      this.toDo[position].toDoText = toDoText;
-      this.displayToDo();
+        this.toDo.splice(position, 1);
+        this.displayToDo();
     
-  },
+    },
   
-  deleteToDo: function(position){
-    
-      this.toDo.splice(position, 1);
-      this.displayToDo();
-    
-  },
+    toggleCompleted: function(position){
   
-  toggleCompleted: function(position){
-  
-      // referencing the specific toDoList we're interested in
-  	  var todo = this.toDo[position];
+        // referencing the specific toDoList we're interested in
+        var todo = this.toDo[position];
   	
-      // we grab what it is, then we flip it with the bang operator (!)
-      todo.completed = !todo.completed;
-	  this.displayToDo();
+        // we grab what it is, then we flip it with the bang operator (!)
+        todo.completed = !todo.completed;
+        this.displayToDo();
     
-  }
+    }
 
 };
 
